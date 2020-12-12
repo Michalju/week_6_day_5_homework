51b180e3568db5db1fca1c422f2b2419d98df7a7
Michalju <juszkin@gmail.com>
2020-12-12 21:09:25 +0000

    tidy up the code

--- js/app.js
+++ js/app.js
@@ -7,19 +7,29 @@ document.addEventListener('DOMContentLoaded', () => {
   })
 
   const handleDogFormSubmit = function (event) {
+    
     event.preventDefault();
-    console.log(event.target.name.value);
-    console.log(event.target.kids_friendly.value);
-    const newdiv= document.createElement('div');
-    newdiv.setAttribute("id", event.target.name.value);
+
+    // sets new div under which the form items will be placed
+    const newDiv= document.createElement('div');    
+
+
+    // creates paragraph with the breed name
     const newName= document.createElement('p');
     newName.textContent=  `Breed : ${event.target.name.value}`;
+
+    // creates paragraph with the kids friendly rating
     const newKidsFriendly= document.createElement('p');
     newKidsFriendly.textContent= `Kids friendly rating: ${event.target.kids_friendly.value}`;
+
+    //appends h2 with new div
     const h2 = document.querySelector("h2");
-    h2.appendChild(newdiv);
-    const div= document.querySelector(`#${event.target.name.value}`);
-    div.appendChild(newName);
-    div.appendChild(newKidsFriendly);
+    h2.appendChild(newDiv);
+
+    //adds paragraphs under newly created div
+    newDiv.appendChild(newName);
+    newDiv.appendChild(newKidsFriendly);
+    
+    //resets form data
     document.getElementById("dog_form").reset();
   }
