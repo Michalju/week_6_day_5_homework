362f05eb05b524dab2887b401d3a0fde109d5039
Michalju <juszkin@gmail.com>
2020-12-12 21:05:17 +0000

    created functionality to add items to list

--- js/app.js
+++ js/app.js
@@ -8,6 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const handleDogFormSubmit = function (event) {
     event.preventDefault();
-    console.log('hi');
-
+    console.log(event.target.name.value);
+    console.log(event.target.kids_friendly.value);
+    const newdiv= document.createElement('div');
+    newdiv.setAttribute("id", event.target.name.value);
+    const newName= document.createElement('p');
+    newName.textContent=  `Breed : ${event.target.name.value}`;
+    const newKidsFriendly= document.createElement('p');
+    newKidsFriendly.textContent= `Kids friendly rating: ${event.target.kids_friendly.value}`;
+    const h2 = document.querySelector("h2");
+    h2.appendChild(newdiv);
+    const div= document.querySelector(`#${event.target.name.value}`);
+    div.appendChild(newName);
+    div.appendChild(newKidsFriendly);
+    document.getElementById("dog_form").reset();
   }
